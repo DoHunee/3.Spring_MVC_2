109fa8261af3f7527bbbd41788d412da747e8cd2
장도훈 <jehgns1@gmail.com>
2024-08-10 14:44:52 +0900

    요청매핑

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -1,6 +1,6 @@
 package hello.springmvc;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;	
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
